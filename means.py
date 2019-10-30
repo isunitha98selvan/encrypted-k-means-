ab4b685587312996de14e544739b60e6607a4ae7
RosaGeorge31 <rosageorge97@gmail.com>
2019-10-30 14:54:03 +0530

    Modified means

--- means.py
+++ means.py
@@ -3,35 +3,51 @@ verbose = False
 
 class KMeans:
 
-    def __init__(self, k, inputFile):
-        self.dataPoints = []
-        if verbose:
-            print inputFile
-        f = open(inputFile, 'r')
-        for line in f:
-            val = eval(line)
-            self.d = len(val)
-            self.dataPoints.append(DataPoint(len(val), val))
-        self.k = k
-        self.means = self.dataPoints[:k]
+	def __init__(self, k, inputFile):
+		self.dataPoints = []
+		if verbose:
+			print inputFile
+		f = open(inputFile, 'r')
+		for line in f:
+			val = eval(line)
+			self.d = len(val)
+			self.dataPoints.append(DataPoint(len(val), val))
+		self.k = k
+		self.means = self.dataPoints[:k]
 
-    def getClusterCentroids(self):
-        centroids = [[DataPoint(self.d),0] for i in range(self.k)]
-        for point in self.dataPoints:
-            closestCluster =min([(self.means[i].distanceTo(point), i) for i in range(self.k)])[1]
-            point.setCluster(self.means[closestCluster])
-            centroids[closestCluster][0].addVector(point)
-            centroids[closestCluster][1]+=1
-        return centroids
+	def getClusterCentroids(self):
+		centroids = [[DataPoint(self.d),0] for i in range(self.k)]
+		for point in self.dataPoints:
+			closestCluster =min([(self.means[i].distanceTo(point), i) for i in range(self.k)])[1]
+			point.setCluster(self.means[closestCluster])
+			centroids[closestCluster][0].addVector(point)
+			centroids[closestCluster][1]+=1
+		return centroids
 
-    def updateMeans(self, means):
-        self.means = means
+	def updateMeans(self, means):
+		self.means = means
 
-    def __str__(self):
-        return str([mean.getVector() for mean in self.means])
+	def allPoints(self, numClusters):
+		final_data = [[] for i in range(numClusters)]
+		for point in self.dataPoints:
+			final_data[point.getCluster()].append(point)
+		datafile = "result/clusters/"
+		if os.path.exists(datafile):
+			os.remove(datafile)
+		f = open(datafile, 'w')
+		count = 0
+		for data in final_data:
+			count +=1
+			datafile = "result/clusters/"+str(count)+".in"
+			f = open(datafile, 'a')
+			f.write("%s\n" % data)
+			f.close()
 
-    def getDims(self):
-        return self.d
+	def __str__(self):
+		return str([mean.getVector() for mean in self.means])
+
+	def getDims(self):
+		return self.d
 
 
 
